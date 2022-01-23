3f4eb642cdcf46d9d6e00bf0e0013c54fc629f58
ittamil <69399378+ittamil@users.noreply.github.com>
2022-01-23 18:31:53 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -123,8 +123,8 @@ LOGOUT_REDIRECT_URL = "login"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'harisuku00@gmail.com'
-EMAIL_HOST_PASSWORD = 'harimitnickhari' 
+EMAIL_HOST_USER = 'main'
+EMAIL_HOST_PASSWORD = 'password' 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
